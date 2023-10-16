38e8df28985253087a291f7398a085d1b6d72d60
tiutiutiu666 <farrellytheoariela@gmail.com>
2023-10-16 13:52:01 +0700

    Fixed shuriken and collision

--- TestWorld.java
+++ TestWorld.java
@@ -32,48 +32,15 @@ public class TestWorld extends UWorld
         AddObject(spike, 732, 836);
         spike.SetDefaultLocation(732, 836);
 
-        Ground ground = new Ground();
-        AddObject(ground,275,610);
-        Ground ground2 = new Ground();
-        AddObject(ground2,381,610);
-        Ground ground3 = new Ground();
-        AddObject(ground3,487,610);
-        Ground ground4 = new Ground();
-        AddObject(ground4,593,611);
-        Ground ground5 = new Ground();
-        AddObject(ground5,700,612);
-        Ground ground6 = new Ground();
-        AddObject(ground6,807,613);
-        Ground ground7 = new Ground();
-        AddObject(ground7,913,613);
-        Ground ground8 = new Ground();
-        AddObject(ground8,1020,613);
-        Ground ground9 = new Ground();
-        AddObject(ground9,1127,613);
-        Ground ground10 = new Ground();
-        AddObject(ground10,1234,613);
-        Ground ground11 = new Ground();
-        AddObject(ground11,1709,909);
-        Ground ground12 = new Ground();
-        AddObject(ground12,1833,909);
-        ground3.setLocation(272,899);
-        ground4.setLocation(378,899);
-        ground5.setLocation(471,899);
-        ground.setLocation(73,908);
-        ground2.setLocation(147,908);
-        ground12.setLocation(1814,912);
-        ground6.setLocation(719,902);
-        ground7.setLocation(957,902);
-        ground8.setLocation(1212,897);
-        ground9.setLocation(1404,897);
-        ground10.setLocation(1537,902);
-        ground8.setLocation(1180,882);
-        Ground ground13 = new Ground();
-        AddObject(ground13,1249,891);
-        ground8.setLocation(1143,893);
-        ground13.setLocation(1249,891);
-        
         Gear gear = new Gear();
         AddObject(gear,368,695);
+
+        Platform1 platform1 = new Platform1();
+        AddObject(platform1,132,851);
+
+        Platform1 platform2 = new Platform1();
+        AddObject(platform2,264,851);
+
+        AddObject(new STrow(0, 10), 264, 820);
     }
 }
